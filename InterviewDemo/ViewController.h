e6a973e67e0861702d4fb2bea1261a3926332d79
daiyu2015 <daiyu2015@gmail.com>
2015-07-12 22:39:02 +0800

    将代码托管到github

---
+++ InterviewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  InterviewDemo
+//
+//  Created by 代雨 on 15/7/11.
+//  Copyright (c) 2015年 代雨. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ InterviewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  InterviewDemo
+//
+//  Created by 代雨 on 15/7/11.
+//  Copyright (c) 2015年 代雨. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
